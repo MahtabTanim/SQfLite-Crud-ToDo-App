a1ce74f3d341885490dda5d2ff9d8889b0c04704
MahtabTanim <tanimbsmrstu@gmail.com>
2021-10-20 19:27:43 +0600

    Initital Commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:your_tasks/screens/home_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "To Do",
+    home: HomePage(),
+  ));
+}
